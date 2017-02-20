eeec69bb4513b555acdfab731a252ca57c0e9cf1
${USER_NAME} <wizardofmath@gmail.com>
2017-02-19 18:52:47 -0700

    Implement time for mac.

--- activity/watcher_darwin.go
+++ activity/watcher_darwin.go
@@ -1,6 +1,9 @@
 package activity
 
 import (
+	"os/exec"
+	"strconv"
+	"strings"
 	"sync"
 	"time"
 )
@@ -16,77 +19,39 @@ type Watcher struct {
 }
 
 // NewWatcher returns a watcher that can be used to get user activity.
-func newDarwinWatcher(activeThreshold, pollFrequency time.Duration) Monitor {
+func newDarwinWatcher(activeThreshold, pollFrequency time.Duration) (Monitor, error) {
 	w := Watcher{}
 	w.activeThreshold = activeThreshold
 	w.pollFrequency = pollFrequency
-	go w.timeUpdater()
+	_, err := w.TimeSinceLastInput()
+	if err != nil {
+		return nil, err
+	}
 	go w.activityPoller()
-	return &w
+	return &w, nil
 }
 
 func (w *Watcher) TimeSinceLastInput() (time.Duration, error) {
-	// Implement for osx
-	return 0, ErrFailedToGetLastInput
-}
-
-func (w *Watcher) timeUpdater() {
-	for {
-		time.Sleep(w.pollFrequency)
+	cmd := exec.Command("ioreg", "-c", "IOHIDSystem")
+	o, err := cmd.Output()
+	if err != nil {
+		return 0, err
 	}
-}
-
-func (w *Watcher) IsActive() bool {
-	t, _ := w.TimeSinceLastInput()
-	return t < w.activeThreshold
-}
-
-func (w *Watcher) IsInActive() bool {
-	t, _ := w.TimeSinceLastInput()
-	return t > w.activeThreshold
-}
-
-// UserActiveChan returns a chan that when an empty struct is received
-// means that a user is active.
-func (w *Watcher) UserActiveChan() chan struct{} {
-	w.Lock()
-	a := make(chan struct{})
-	w.active = append(w.active, a)
-	w.Unlock()
-	return a
-}
-
-// activityPoller checks to see if the user is inactive or active if it is
-func (w *Watcher) activityPoller() {
-	var active bool
-	var previousActive bool
-	for {
-		time.Sleep(w.pollFrequency)
-		active = w.IsActive()
-		if active != previousActive {
-			switch {
-			case active:
-				for _, a := range w.active {
-					a <- struct{}{}
-				}
-			case !active:
-				for _, a := range w.inactive {
-					a <- struct{}{}
-				}
-			}
-		}
-		previousActive = active
+	// o := run("cat", "ioreg")
+	const idleTime = `"HIDIdleTime" = `
+	const idleN = len(idleTime)
+	n := strings.Index(string(o), idleTime)
+	cutOff := n + 50
+	if cutOff > len(o) {
+		cutOff = len(o) - 1
 	}
-}
-
-// UserInActiveChan returns a chan that when an empty struct is received
-// means that a user is inactive.
-func (w *Watcher) UserInactiveChan() chan struct{} {
-	a := make(chan struct{})
-	w.Lock()
-	w.inactive = append(w.inactive, a)
-	w.Unlock()
-	return a
+	s := strings.Index(string(o[n:cutOff]), "\n")
+	o = o[n+idleN : n+s]
+	i, err := strconv.ParseInt(string(o), 10, 64)
+	if err != nil {
+		return 0, err
+	}
+	return time.Duration(i/1000000) * time.Millisecond, nil
 }
 
 func init() {
